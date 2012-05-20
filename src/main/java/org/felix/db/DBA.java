c9bd9ce44c603716dd3991423e5e890139196e0e
guoguibing <guoguibing@localhost>
2012-05-20 16:26:45 +0800

    general update

--- src/main/java/org/felix/db/DBA.java
+++ src/main/java/org/felix/db/DBA.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -21,11 +22,11 @@ import au.com.bytecode.opencsv.CSVReader;
 public class DBA
 {
 	private static Logger logger = null;
-	private final static String	DRIVER	= "org.apache.derby.jdbc.EmbeddedDriver";
-	private final static String	URL		= "jdbc:derby:BookDB;create=true";
-
-	private Connection			conn;
-	private Statement			stmt;
+	private final static String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+	private final static String URL = "jdbc:derby:BookDB;create=true";
+	
+	private Connection conn;
+	private Statement stmt;
 	
 	public DBA()
 	{
@@ -43,19 +44,17 @@ public class DBA
 		{
 			logger.error(e.getMessage());
 		}
-
-		String SQL_table = "CREATE TABLE books"
-			+ "(isbn VARCHAR(100) PRIMARY KEY," +
-			  "title VARCHAR(200) NOT NULL,"+
-			  "authors VARCHAR(255) NOT NULL," +
-			  "pubYear INTEGER," +
-			  "publisher VARCHAR(255)," +
-			  "imgUrlS VARCHAR(255)," +
-			  "imgUrlM VARCHAR(255)," +
-			  "imgUrlL VARCHAR(255)" +
-			  " )";
+		
+		String SQL_table = "CREATE TABLE books" + "(isbn VARCHAR(100) PRIMARY KEY," + "title VARCHAR(200) NOT NULL,"
+				+ "authors VARCHAR(255) NOT NULL," + "pubYear INTEGER," + "publisher VARCHAR(255),"
+				+ "imgUrlS VARCHAR(255)," + "imgUrlM VARCHAR(255)," + "imgUrlL VARCHAR(255)" + " )";
 
 		return stmt.execute(SQL_table);
+	}
+	
+	private String replace(String old)
+	{
+		return old.replace('\'', '\"');
 	}
 
 	public void insertData() throws Exception
@@ -68,21 +67,35 @@ public class DBA
 		while ((data = br.readNext()) != null)
 		{
 			isbn = data[0];
-			title = data[1];
-			authors = data[2];
-			year = Integer.parseInt(data[3]);
-			publisher = data[4];
-			imgUrlS = data[5];
-			imgUrlM = data[6];
-			imgUrlL = data[7];
+			title = replace(data[1]);
+			authors = replace(data[2]);
 
+			year = Integer.parseInt(data[3]);
+			publisher = replace(data[4]);
+			imgUrlS = replace(data[5]);
+			imgUrlM = replace(data[6]);
+			imgUrlL = replace(data[7]);
+			
+			/* if already exists, skip it */
+			String SQL_query = "SELECT * FROM books where isbn = '" + isbn + "'";
+			ResultSet rs = stmt.executeQuery(SQL_query);
+			if (rs.next()) continue;
+
+			/* otherwise, insert data into table */
 			String SQL_insert = new StringBuilder()
 					.append("INSERT INTO books (isbn, title, authors, pubYear, publisher, imgUrlS, imgUrlM, imgUrlL) VALUES ('")
 					.append(isbn).append("', '").append(title).append("', '").append(authors).append("', ")
 					.append(year).append(", '").append(publisher).append("', '").append(imgUrlS).append("', '")
 					.append(imgUrlM).append("', '").append(imgUrlL).append("')").toString();
 
-			stmt.executeUpdate(SQL_insert);
+			try
+			{
+				stmt.executeUpdate(SQL_insert);
+			} catch (Exception e)
+			{
+				logger.debug(SQL_insert);
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -96,8 +109,6 @@ public class DBA
 	{
 		Class.forName(DRIVER).newInstance();
 		conn = DriverManager.getConnection(URL);
-		logger.error("Connection is created.");
-
 		stmt = conn.createStatement();
 	}
 
@@ -106,8 +117,10 @@ public class DBA
 		DBA dba = new DBA();
 
 		dba.getConn();
-		dba.createTable();
+		// dba.createTable();
 		dba.insertData();
 		dba.closeConn();
+		
+		logger.error("Program is Finished!");
 	}
 }
