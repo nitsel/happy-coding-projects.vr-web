37bba51d78781ed1c5b13a1beba73ff37cd14b01
guoguibing <guoguibing@localhost>
2012-05-25 17:57:00 +0800

    generally update

--- src/main/java/org/felix/db/BookDao.java
+++ src/main/java/org/felix/db/BookDao.java
@@ -1,18 +1,76 @@
 package org.felix.db;
 
-/**
- * Database access object for Book class
- * 
- * @author guoguibing
- * 
- */
-@SuppressWarnings("hiding")
-public class BookDao<Book> extends DefaultDao<Book>
-{
+import java.sql.ResultSet;
 
-	@Override
-	public void update(Book book)
+public class BookDao extends Dao
+{
+	private final static String	tableElements	= "isbn, isbn13, googleId, title, subTitle, authors, "
+														+ "pages, price, language, publishDate, publisher, edition, dimensions, weight, description, "
+														+ "editorReviews, imgUrlS, imgUrlM, imgUrlL, amazonUrl, reviewUrl, averageRating, ranking";
+
+	void update(Book book)
+	{};
+
+	void insert(Book book) throws Exception
 	{
+		// check if it is already exist
+		ResultSet rs = query(book);
+		if (rs.next())
+		{
+			logger.debug("Book (isbn = {}) has already been existing in the database. ", book.getIsbn());
+			return;
+		}
+
+		// insert into database
+		String sql = "INSERT INTO books (" + tableElements + ") VALUES ('" + book.getIsbn() + "', '" 
+				+ book.getIsbn13() + "', '" + book.getGoogleId() + "', '" + book.getTitle() + "', '" 
+				+ book.getSubTitle() + "', '" + book.getAuthors() + "', " + book.getPages() + ", '" 
+				+ book.getPrice() + "', '" + book.getLanguage()	+ "', " + book.getPublishDate() + ", '" 
+				+ book.getPublisher() + "', " + book.getEdition() + ", '"
+				+ book.getDimensions() + "', " + book.getWeight() + ", '" + book.getDescription() + "', '"
+				+ book.getEditorReviews() + "', '" + book.getImgUrlS() + "', '" + book.getImgUrlM() + "', '"
+				+ book.getImgUrlL() + "', '" + book.getReviewUrl() + "', " + book.getAverageRating() + ", "
+				+ book.getRanking() + ", ";
+
+		logger.debug("Insert book: {}", sql);
+
+		stmt.executeUpdate(sql);
+	};
+
+	public void delete(Book book) throws Exception
+	{
+		String sql = "DELETE FROM books WHERE isbn = '" + book.getIsbn() + "'";
+
+		logger.debug("Delete book: {}", sql);
+		stmt.executeUpdate(sql);
+	};
+
+	public ResultSet query(Book book) throws Exception
+	{
+		String sql = "SELECT * FROM books WHERE isbn = '" + book.getIsbn() + "'";
+
+		logger.debug("Query book: {}", sql);
+		return stmt.executeQuery(sql);
 	}
 
+	@Override
+	public boolean createTable() throws Exception
+	{
+
+		String sql = "CREATE TABLE books (isbn VARCHAR(20) PRIMARY KEY, isbn13 VARCHAR(20), googleId VARCHAR(30), title VARCHAR(500) NOT NULL,"
+				+ "subTitle VARCHAR(500), authors VARCHAR(200), pages INTEGER, price VARCHAR(50), language VARCHAR(20), publishDate DATE, publisher VARCHAR(300),"
+				+ "edition INTEGER, dimensions VARCHAR(100), weight VARCHAR(100), description VARCHAR(2000), editorReviews VARCHAR(2000), "
+				+ "imgUrlS VARCHAR(500), imgUrlM VARCHAR(500), imgUrlL VARCHAR(500), amazonUrl VARCHAR(500), reviewUrl(500), averageRating REAL, ranking VARCHAR(100) )";
+
+		logger.debug("Create table books: {}", sql);
+		return stmt.execute(sql);
+
+	};
+
+	@Override
+	protected boolean dropTable() throws Exception
+	{
+		String sql = "DROP TABLE books";
+		return stmt.execute(sql);
+	}
 }
--- src/main/java/org/felix/db/Dao.java
+++ src/main/java/org/felix/db/Dao.java
@@ -1,12 +1,132 @@
 package org.felix.db;
 
-public interface Dao<T>
+import java.io.File;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import org.felix.web.client.AmazonBookClient;
+import org.felix.web.client.GoogleBookClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+/**
+ * Database Administrator Operator: prepare data tables and initial data
+ * 
+ * @author guoguibing
+ * 
+ */
+public abstract class Dao
 {
-	abstract void update(T t);
+	protected static Logger		logger	= null;
+	private final static String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+	private final static String URL = "jdbc:derby:BookDB";
+	
+	protected static Connection	conn;
+	protected static Statement	stmt;
+	
+	static
+	{
+		System.setProperty("Password", "through@pass");
+		logger = LoggerFactory.getLogger(Dao.class);
 
-	abstract void insert(T t);
+		getConn();
+	}
 
-	abstract void delete(T t);
+	protected abstract boolean createTable() throws Exception;
 
-	abstract void query(T t);
+	protected abstract boolean dropTable() throws Exception;
+	
+	private String replace(String old)
+	{
+		return old.replace('\'', '\"');
+	}
+
+	public void insertData() throws Exception
+	{
+		String title = null, isbn = null, authors = null, publisher = null, imgUrlS = null, imgUrlM = null, imgUrlL = null;
+		int year = 0;
+		
+		CSVReader br = new CSVReader(new FileReader(new File("Book-Crossing/BX-Books.csv")), ';', '"', 1);
+		String[] data = null;
+		while ((data = br.readNext()) != null)
+		{
+			isbn = data[0];
+			title = replace(data[1]);
+			authors = replace(data[2]);
+
+			year = Integer.parseInt(data[3]);
+			publisher = replace(data[4]);
+			imgUrlS = replace(data[5]);
+			imgUrlM = replace(data[6]);
+			imgUrlL = replace(data[7]);
+			
+			/* if already exists, skip it */
+			String SQL_query = "SELECT * FROM books where isbn = '" + isbn + "'";
+			ResultSet rs = stmt.executeQuery(SQL_query);
+			if (rs.next()) continue;
+
+			/* otherwise, insert data into table */
+			String SQL_insert = new StringBuilder()
+					.append("INSERT INTO books (isbn, title, authors, pubYear, publisher, imgUrlS, imgUrlM, imgUrlL) VALUES ('")
+					.append(isbn).append("', '").append(title).append("', '").append(authors).append("', ")
+					.append(year).append(", '").append(publisher).append("', '").append(imgUrlS).append("', '")
+					.append(imgUrlM).append("', '").append(imgUrlL).append("')").toString();
+
+			try
+			{
+				stmt.executeUpdate(SQL_insert);
+			} catch (Exception e)
+			{
+				logger.debug(SQL_insert);
+				e.printStackTrace();
+			}
+		}
+	}
+
+	protected void closeConn() throws SQLException
+	{
+		if (stmt != null) stmt.close();
+		if (conn != null) conn.close();
+	}
+
+	protected static void getConn()
+	{
+		try
+		{
+			Class.forName(DRIVER).newInstance();
+			conn = DriverManager.getConnection(URL + ";create=true");
+			stmt = conn.createStatement();
+
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		String title = "A Second Chicken Soup for the Woman's";
+		String isbn = "1558746226";
+
+		GoogleBookClient googleBook = new GoogleBookClient();
+		String googleId = googleBook.searchBookId(title, isbn);
+
+		if (googleId != null)
+		{
+			AmazonBookClient amazonBook = new AmazonBookClient();
+			Book book = amazonBook.searchBook(isbn);
+			book.setGoogleId(googleId);
+
+			List<Review> reviews = amazonBook.searchReviews(isbn, book.getReviewUrl());
+
+			// update to database
+		}
+	}
 }
