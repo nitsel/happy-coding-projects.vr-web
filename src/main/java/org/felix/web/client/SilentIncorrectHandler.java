1c2c88d4f21f64e11d4b7d2c6a65744f5d03eadb
guoguibing <guoguibing@localhost>
2012-11-05 10:11:41 +0800

    Daily Update

---
+++ src/main/java/org/felix/web/client/SilentIncorrectHandler.java
@@ -0,0 +1,15 @@
+package org.felix.web.client;
+
+import com.gargoylesoftware.htmlunit.IncorrectnessListener;
+
+public class SilentIncorrectHandler implements IncorrectnessListener
+{
+
+	@Override
+	public void notify(String message, Object origin)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
